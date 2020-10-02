700b3939805513f3e62a284c5ef671b146b41636
Danillo Perez <danillo0101@live.nl>
2020-10-02 11:55:49 +0200

    Initial commit
    initial commit

---
+++ LogicLayer/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LogicLayer
+{
+    public class Animal
+    {
+        public enum AnimalType
+        {
+            Carnivore,
+            Herbivore
+        }
+        public enum AnimalSize
+        {
+            Small = 1,
+            Medium = 3,
+            Large = 5
+        }
+
+        public AnimalSize animalSize { get; set; }
+        public AnimalType animalType { get; set; }
+        
+        public Animal(AnimalType type, AnimalSize size)
+        {
+            this.animalSize = size;
+            this.animalType = type;
+        }
+    }
+}
---
+++ LogicLayer/SortingAlgorithm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLayer
+{
+    public class SortingAlgorithm
+    {
+        public List<Animal> SortAnimals(List<Animal> animalList)
+        {
+            animalList = SortBySize(animalList);
+            animalList = SortByType(animalList);
+            return animalList;
+        }
+        public List<Animal> SortByType(List<Animal> oldAnimals)
+        {
+            List<Animal> carnivoreList = new List<Animal>();
+            List<Animal> herbivoreList = new List<Animal>();
+            foreach(Animal animal in oldAnimals)
+            {
+                if (animal.animalType == Animal.AnimalType.Carnivore)
+                    carnivoreList.Add(animal);
+                else if (animal.animalType == Animal.AnimalType.Herbivore)
+                    herbivoreList.Add(animal);
+            }
+            carnivoreList.AddRange(herbivoreList);
+            return carnivoreList;
+        }
+        public List<Animal> SortBySize(List<Animal> oldAnimals)
+        {
+            List<Animal> returnList = new List<Animal>();
+            foreach (Animal loop in oldAnimals)
+            {
+                Animal tempAnimal = new Animal(Animal.AnimalType.Herbivore, Animal.AnimalSize.Large);
+                foreach(Animal animal in oldAnimals)
+                {
+                    if ((int)animal.animalSize <= (int)tempAnimal.animalSize)
+                    {
+                        returnList.Add(animal);
+                    }
+                }
+            }
+            return returnList;
+        }
+    }
+}
